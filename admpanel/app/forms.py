f2771cdb6b63c933d48aeeafbdca43a03a67209c
lehakot-create <lehakot85@inbox.ru>
2023-04-19 20:16:46 +0400

    искл поле telegram id

--- admpanel/app/forms.py
+++ admpanel/app/forms.py
@@ -7,7 +7,7 @@ class EmployeeForm(forms.ModelForm):
     class Meta:
         model = Employee
         fields = '__all__'
-        exclude = ('uuid',)
+        exclude = ('uuid', 'telegram_user_id')
         widgets = {
             'date_time_contract': forms.DateInput(
                                         format=('%d/%m/%Y'),
